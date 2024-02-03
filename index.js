c060820d44b824696065244ac944d6e8ae012f01
Yousra <nice.sora@hotmail.com>
2024-02-03 16:25:25 +1100

    Updated index.js/generateMarkdown.js files

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  }, {
+    type: "input",
+    name: "name",
+    message: "Enter your full name.",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of your project:",
+  },
+ 
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your application:",
+    choices: ["MIT", "Apache_2.0", "MPL_2.0", "BSD_3--Clause", "None"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm install"
+  },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information:",
+  },
+ 
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test instructions:",
+  },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+//function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error("Error writing README.md:", err);
+    } else {
+      console.log("README.md generated successfully!");
+    }
+  });
+}
 
-// Function call to initialize app
+//function to initialize app
+function init() {
+  inquirer.prompt(questions).then((res) => {
+    writeToFile("README.md", generateMarkdown(res));
+  });
+}
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,82 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+ //function that returns a license badge based on which license is passed in
+//If there is no license, returns an empty string
+function renderLicenseBadge(license) {
+  if (license == "None") {
+    return "";
+  } else if (license == "MIT") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-yellow.svg)`;
+  } else if (license == "MPL_2.0") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-brightgreen.svg)`;
+  }
+  return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function that returns the license link
+//If there is no license, returns an empty string
+function renderLicenseLink(license) {
+  if (license === "BSD_3--Clause") {
+    return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (license === "Apache_2.0") {
+    return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "MIT") {
+    return `[![License](https://img.shields.io/badge/license-${license}-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === "MPL_2.0") {
+    return `[![License](https://img.shields.io/badge/license-${license}-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+//function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  by ${data.name}
+  ${renderLicenseBadge(data.license)} 
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+## Usage
+${data.usage}
+
+## License
+ ${
+   data.license !== "None"
+     ? renderLicenseLink(data.license) +
+       " This project is licensed under the " +
+       data.license +
+       " license."
+     : "N/A"
+ }
+ 
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me:
+
+GitHub: [${data.github}](https://github.com/${data.github})
+
+Email: ${data.email}
 
 `;
 }
